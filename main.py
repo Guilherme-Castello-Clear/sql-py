b81f10ec22bc4387e82af95ed40c87d65512a404
Guilherme-Castello-Clear <guilherme.castello.clear@gmail.com>
2023-11-11 19:51:11 -0300

    Feat: Using sqlalchemy

--- main.py
+++ main.py
@@ -1,35 +1,30 @@
 from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
+from flask_sqlalchemy import SQLAlchemy
 
-db = sqlite3.connect("books-collection.db")
-cursor = db.cursor()
-# cursor.execute("CREATE TABLE books (id INTEGER PRIMARY KEY, title varchar(250) NOT NULL UNIQUE, author varchar(250) NOT NULL, rating FLOAT NOT NULL)")
-cursor.execute("INSERT INTO books VALUES(1, 'Harry Potter', 'J. K. Rowling', '9.3')")
-db.commit()
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///new-books-collection.db"
 
-all_books = []
-
-
-@app.route('/')
-def home():
-    return render_template("index.html", books=all_books)
+db = SQLAlchemy()
+db.init_app(app)
 
 
-@app.route("/add", methods=["GET", "POST"])
-def add():
-    if request.method == "POST":
-        book = {
-            "title": request.form["title"],
-            "author": request.form["author"],
-            "rating": request.form["rating"]
-        }
-        all_books.append(book)
-        return redirect(url_for('home'))
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    rating = db.Column(db.Float, nullable=False)
 
-    return render_template("add.html")
+    # Optional: this will allow each book object to be identified by its title when printed.
+    def __repr__(self):
+        return f'<Book {self.title}>'
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+with app.app_context():
+    db.create_all()
 
+
+with app.app_context():
+    new_book = Book(id=1, title="Harry Potter", author="J. K. Rowling", rating=9.3)
+    db.session.add(new_book)
+    db.session.commit()
